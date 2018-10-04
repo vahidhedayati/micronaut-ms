a7a6680ac9b7a6c3a0b51dd12e6e94ad88e17a4b
Vahid Hedayati <badvad@gmail.com>
2018-10-04 16:56:11 +0100

    tidyup

--- beer-front/src/main/java/micronaut/demo/beer/client/StockFallBack.java
+++ beer-front/src/main/java/micronaut/demo/beer/client/StockFallBack.java
@@ -10,6 +10,9 @@ import micronaut.demo.beer.model.StockEntity;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * When all fails and stocks micro service is not up some default out of stock is returned
+ */
 @Client(id = "stock", path = "/stock")
 @Fallback
 public class StockFallBack implements StockControllerClient {
@@ -17,21 +20,11 @@ public class StockFallBack implements StockControllerClient {
 
     @Override
     public Single list() {
-        System.out.println("Ahh2");
-        //Single<StockEntity> single=null;
         List<StockEntity> ss = new ArrayList<>();
-        ss.add(new StockEntity("out of stock",0L,0.00d,0.00d,0.00d,0.00d) );
+        ss.add(new StockEntity("out of stock",0L,0.00d,0.00d,0.00d,0.00d));
         //ss.add(new StockEntity("out of stock",0L,0.00d,0.00d,0.00d,0.00d) );
-        Single<List<StockEntity>> sss = Single.just(ss);
-        return sss;
-
-        //Single s = Single.just(new StockEntity("out of stock",0L,0.00d,0.00d,0.00d,0.00d) );
-
-        //return Single.zip(sss,sss,(v,v1)->{
-         //   return v1;
-        //});
-
-
+        //Single<List<StockEntity>> sss = Single.just(ss);
+        return Single.just(ss);
     }
 
     @Override
